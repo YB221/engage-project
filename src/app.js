afef08e1f72ab077ea516c6110f699fa712da2e1
yb221 <yashbhambhu.18je0949@am.iitism.ac.in>
2021-07-10 01:51:12 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,92 @@
+let express = require( 'express' );
+let html = require('html');
+const url = require('url');
+//const jsdom = require("jsdom");
+//const { JSDOM } = jsdom;
+//global.window = new JSDOM(html).window;
+const sessionStorage = require('node-sessionstorage');
+let app = express();
+let server = require( 'http' ).Server( app );
+let io = require( 'socket.io' )( server );
+console.log(server);
+const passport = require('passport');
+require('./passport-setup');
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('views'));
+
+// Auth middleware that checks if the user is logged in
+const isLoggedIn = (req, res, next) => {
+
+    if (req.user) {
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+    console.log("req");
+}
+
+// Initializes passport and passport sessions
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Example protected and unprotected routes
+var room_url="";
+app.get('/', (req, res) => {
+
+    
+    room_url=`${req.protocol}://${req.get("host")}${req.originalUrl}`;
+    
+    
+    if(req.user)
+    {
+    res.redirect('/good');
+    }
+    else
+    res.render('./home.html');
+
+})
+
+app.get('/failed', (req, res) => {
+    //res.send(`FAILEDDDDDDDDDDDDDDDDDDDDXD`);
+    res.render('./home.html');
+})
+
+// In this route you can see that if the user is logged in u can acess his info in: req.user
+//app.get('/good', isLoggedIn, (req, res) => res.send(`Welcome mr ${req.user.displayName}!`))
+
+// Auth Routes
+app.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/good');
+  }
+);
+
+app.get('/logout', function(req, res) {
+    console.log("logged out!");
+    req.logout();
+    res.redirect('/');
+});
+
+let stream = require( './emit' );
+let path = require( 'path' );
+let favicon = require( 'serve-favicon' );
+
+app.use( favicon( path.join( __dirname, 'favicon.ico' ) ) );
+app.use( '/assets', express.static( path.join( __dirname, 'assets' ) ) );
+
+app.get( '/good', ( req, res ) => {
+    
+    //module.exports={yourName};
+    
+    res.render( __dirname + '/index.html',{yourName:req.user.displayName,room_url:room_url});
+
+} );
+
+
+io.of( '/stream' ).on( 'connection', stream );
+
+server.listen( 5000 );
---
+++ src/assets/js/script.js
@@ -0,0 +1,69 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    const showNavbar = (toggleId, navId, bodyId, headerId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId)
+    
+    // Validate that all variables exist
+    if(toggle && nav && bodypd && headerpd){
+    toggle.addEventListener('click', ()=>{
+    // show navbar
+    nav.classList.toggle('show')
+    // change icon
+    toggle.classList.toggle('bx-x')
+    // add padding to body
+    bodypd.classList.toggle('body-pd')
+    // add padding to header
+    headerpd.classList.toggle('body-pd')
+   
+    })
+    }
+    }
+    
+    showNavbar('header-toggle','nav-bar','body-pd','header')
+    
+    /*===== LINK ACTIVE =====*/
+    const linkColor = document.querySelectorAll('.nav_link')
+    
+    function colorLink(){
+    if(linkColor){
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+    }
+    }
+    linkColor.forEach(l=> l.addEventListener('click', colorLink))
+    
+    // Your code to run since DOM is loaded and ready
+    });
+
+$("#dashboard").click(()=>{
+    $(".dash-board").show()
+    console.log("clicked");
+})
+
+$("#chats").click(()=>{
+    $(".dash-board").hide()
+    console.log("clicked");
+})
+$('.exit').click(()=>{
+    sessionStorage.removeItem('room_url')
+})
+
+
+
+$('.copy-btn').click(() =>{
+    var copyText = document.getElementById("copyClipboard");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    $('#copied-success').fadeIn(800);
+    $('#copied-success').fadeOut(800);
+  })
+
+
+
+
+
