19a9c6b833dccf65eb22d5ec00393e65f487d885
yb221 <yashbhambhu.18je0949@am.iitism.ac.in>
2021-07-11 03:50:21 +0530

    heroku update

--- src/assets/js/events.js
+++ src/assets/js/events.js
@@ -125,3 +125,23 @@ window.addEventListener( 'load', () => {
         helpers.toggleModal( 'recording-options-modal', false );
     } );
 } );
+
+
+window.addEventListener( 'load', () => {
+    
+    //When the 'Return button' button is clicked.
+    document.getElementById('return-button').addEventListener('click', (e)=>{
+        e.preventDefault();
+        
+            sessionStorage.removeItem('room');
+            location.replace('/good');
+    });
+    
+    //When the 'Logout button' button is clicked.
+    document.getElementById('logout-button').addEventListener('click', (e)=>{
+        e.preventDefault();
+        
+            sessionStorage.removeItem('room');
+            location.replace('/logout');
+    });
+} );
